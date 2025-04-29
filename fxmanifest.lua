3625e32cc3c361e21836c311cd9ce056dbd3e4dd
Alonya <96838192+AlonyaJS@users.noreply.github.com>
2025-04-29 16:37:48 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'Boilerplate'
+author 'Alonya'
+description 'Do resource'
+
+lua54 'yes'
+
+client_script 'dist/client/*.js'
+server_script 'dist/server/*.js'
