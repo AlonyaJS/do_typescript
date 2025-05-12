6d9072069852025282b1d05e986bb43e048aae69
Alonya <96838192+AlonyaJS@users.noreply.github.com>
2025-05-13 01:30:57 +0200

    Delete src directory

--- src/client/client.ts
+++
@@ -1,6 +0,0 @@
-on("onClientResourceStart", (resourceName: string) => {
-  if (resourceName === GetCurrentResourceName()) {
-    console.log("Client Script Started!");
-    emitNet("logToServer", "Client resource loaded!");
-  }
-});
--- src/server/server.ts
+++
@@ -1,5 +0,0 @@
-onNet("logToServer", (message: string) => {
-  const playerId: number = global.source;
-
-  console.log(`Player ${playerId} says: ${message}`);
-});
